121a50d51d5b98a9a1a856e72468b5a709241558
Wilder León <wilder_eduardo85@hotmail.com>
2021-08-03 12:34:52 -0400

    backend routes basic setup created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import ticketsRoutes from './routes/tickets.js';
+import mechanicsRoutes from './routes/mechanics.js';
 
 const app = express();
 
 app.get('/', (req, res)=>{
     res.json({msg: 'Hello World'})
 });
+
+// Routes:
+app.use('/api/v1/tickets', ticketsRoutes);
+app.use('/api/v1/mechanics', mechanicsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
