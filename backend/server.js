5bc27981ed65d024730b8de9862c6bd51495a44e
Wilder León <wilder_eduardo85@hotmail.com>
2021-08-05 15:11:32 -0400

    prepare for deploy in heroku

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ticketsRoutes from './routes/tickets.js';
 import mechanicsRoutes from './routes/mechanics.js';
 import connectDB from './config/db.js';
+import path from 'path'
 
 //Initializations
 const app = express();
@@ -15,6 +16,18 @@ app.use(express.json({extended: false}));
 // Routes:
 app.use('/api/v1/tickets', ticketsRoutes);
 app.use('/api/v1/mechanics', mechanicsRoutes);
+
+// Set current path
+const __dirname = path.resolve();
+
+// Heroku configuration
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
